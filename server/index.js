c79cd612114840fe875afd2684f080ba5e1faa12
Richard Taylor <richardta@kainos.com>
2017-09-04 16:10:56 +0100

    Build upon initial skeleton of express app, added npm start script. Added and configured following js libraries cors, errorhandler and morgan.

--- server/index.js
+++ server/index.js
@@ -1,18 +1,37 @@
-const express = require ('express');
-const app = express();
+/**
+ * Module dependencies.
+ */
+const express = require('express');
+const errorHandler = require('errorhandler');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 const db = require('./db.js');
 const config = require('./config.json');
-const bodyParser = require('body-parser');
 
+/**
+ * Create Express server.
+ */
+const app = express();
+
+/**
+ * Express configuration.
+ */
+app.set('port', config.port || 8002);
+app.use(cors());
 app.use(bodyParser.json());
+app.use(morgan('dev')); // HTTP request logger middleware.
+app.use(errorHandler());// Error Handler middleware for more verbose errors
 
+/**
+ * Routes configuration.
+ */
 app.get('/testQuery', function(req, res) {
 	db.testQuery( function(rows) {
-		res.send(rows)
+		res.send(rows);
 	});
 })
 
-
-app.listen(config.port, function () {
-	console.log('Express API listening on port 8002...')
+app.listen(8002, function() {
+    console.log('Express server listening on port 8002');
 });
