fd3fb90b36d4522db2d0225c2bd20be917cbd5f5
Mary-Jane <mary-janemc@kainos.com>
2017-09-05 11:24:55 +0100

    Added dependency injection to the app.component.spec.ts tests and added DataService dependency back into app.component.ts

--- client/src/app/components/app.component.spec.ts
+++ client/src/app/components/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import { AlertModule } from "ngx-bootstrap";
@@ -6,36 +6,39 @@ import { AlertModule } from "ngx-bootstrap";
 import { HttpClient } from "@angular/common/http";
 import { HttpClientModule } from "@angular/common/http";
 
+import {DataService} from "../services/data.service";
+
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     
     TestBed.configureTestingModule({
+        schemas: [ NO_ERRORS_SCHEMA ],
         declarations: [
             AppComponent
           ],
           imports: [
             AlertModule.forRoot(),
             HttpClientModule,
+          ],
+          providers: [
+              {provide: DataService }
           ]
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
+  it('should create the app', inject([DataService], async(() => {
+    let service = TestBed.get(DataService);
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
+  })));
 
-  it(`should have as title 'Evolve patient portal'`, async(() => {
+  it(`should have as title 'Evolve Patient Portal'`, inject([DataService], async(() => {
+    let service = TestBed.get(DataService);
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Evolve patient portal');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Evolve patient portal!');
-  }));
+    expect(app.title).toEqual('Evolve Patient Portal');
+  })));
 });
