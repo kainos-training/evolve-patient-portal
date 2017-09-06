9e2e7ab168a19dd9001227555989a71f965d9e77
Eduard <eduardg@kainos.com>
2017-09-06 11:36:54 +0100

    Connecting cookies with login components.

--- client/src/app/components/app.component.ts
+++ client/src/app/components/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { DataService } from "../services/data.service";
+import {Component, OnInit} from '@angular/core';
+import {DataService} from '../services/data.service';
 import * as $ from 'jquery';
+import {User} from '../user';
+import {SwitchBoardService} from '../services/switch-board.service';
 
 
 @Component({
@@ -9,7 +11,31 @@ import * as $ from 'jquery';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'Evolve Patient Portal';
+    user: User;
 
-    constructor(private data: DataService) { }
+    constructor(private data: DataService, private switchBoard: SwitchBoardService) {
+        // $(function() {
+        //     console.log('jquery is working');
+        // });
+    }
+
+    ngOnInit(): void {
+        // create a default user
+        this.resetUser();
+        // check for previous data stored in cookie
+        this.data.getUserFromCookie(this.user);
+        // if no data is found, this.user will only be subscribed to the switchboard
+    }
+
+    resetUser() {
+        this.user = new User();
+        this.user.loggedIn = false;
+    }
+
+    logout() {
+        // make sure it will redirect to login page
+
+        this.data.removeCookie();
+        this.ngOnInit();
+    }
 }
--- client/src/app/user.ts
+++ client/src/app/user.ts
@@ -3,14 +3,6 @@ export class User {
     username: string;
     password: string;
     token: string;
-    salt: string;
-    dateOfBirth: Date;
-    gender: string;
-    mrin: string;
-    firstName: string;
-    lastName: string;
-    phoneNumber: string;
-    title: string;
-    address: string;
-    email: string;
+    loggedIn: boolean;
+    message: string;
 }
