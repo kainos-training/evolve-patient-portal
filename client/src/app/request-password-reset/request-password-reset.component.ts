f4bb59d0505594cf56a98f8f77179712f6a86d53
Mary-Jane <mary-janemc@kainos.com>
2017-09-07 12:29:24 +0100

    Fixed bug on request password reset to handle empty username field

--- client/src/app/request-password-reset/request-password-reset.component.ts
+++ client/src/app/request-password-reset/request-password-reset.component.ts
@@ -1,65 +1,60 @@
-import { Component, OnInit, Input, OnDestroy} from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { DataService } from '../services/data.service'
 import { Location } from '@angular/common';
 import { User } from '../user';
-import {Subscription} from 'rxjs/Subscription';
-import {SwitchBoardService} from '../services/switch-board.service';
-import {Router} from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { SwitchBoardService } from '../services/switch-board.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'evolve-request-password-reset',
-  templateUrl: './request-password-reset.component.html',
-  styleUrls: ['./request-password-reset.component.css']
+    selector: 'evolve-request-password-reset',
+    templateUrl: './request-password-reset.component.html',
+    styleUrls: ['./request-password-reset.component.css']
 })
 export class RequestPasswordResetComponent implements OnInit, OnDestroy {
 
-  public evolveLogoPath: string;
-  valid : Boolean;
-  successful: Boolean;
-  dataService: DataService;
-  user: User;
-  validSubscription: Subscription;
-  successfulSubscription: Subscription;
-  emailRequested: boolean;
+    public evolveLogoPath: string;
+    valid: Boolean;
+    successful: Boolean;
+    dataService: DataService;
+    user: User;
+    validSubscription: Subscription;
+    successfulSubscription: Subscription;
 
-  
-  constructor(dataService: DataService, private switchBoard: SwitchBoardService, private router: Router) {
-    this.user = new User();
-    this.valid = true;
-    this.successful = false;
-    this.dataService = dataService;
-    this.evolveLogoPath = 'assets/EvolveLogo.svg';
-    this.emailRequested = false;
-  }
 
-  onRequestReset(): void
-  {
-    console.log('reaches onRequestReset() and Username is: ' + this.user.username);
-    //this.dataService.requestReset(this.username); 
-    this.dataService.requestResetPassword(this.user, this.router);
-    this.emailRequested = true;
-    //alert("dataService.requestReset returned " + this.successful);
-  }
+    constructor(dataService: DataService, private switchBoard: SwitchBoardService, private router: Router) {
+        this.user = new User();
+        this.valid = true;
+        this.successful = false;
+        this.dataService = dataService;
+        this.evolveLogoPath = 'assets/EvolveLogo.svg';
+    }
 
-  onGoBack(): void
-  {
-    //alert('User clicked cancel');
-    //this.location.back();
-  }
-  ngOnInit(): void {
-    this.validSubscription = this.switchBoard.valid$.subscribe((v) => {
-      this.valid = v;
-    });
+    onRequestReset(): void {
+        console.log('reaches onRequestReset() and Username is: ' + this.user.username);
+        //this.dataService.requestReset(this.username); 
+        this.dataService.requestResetPassword(this.user, this.router);
+        //alert("dataService.requestReset returned " + this.successful);
+    }
 
-    this.successfulSubscription = this.switchBoard.successful$.subscribe((s) => {
-      this.successful = s;
-    });
+    onGoBack(): void {
+        //alert('User clicked cancel');
+        //this.location.back();
+    }
+    ngOnInit(): void {
+        this.validSubscription = this.switchBoard.valid$.subscribe((v) => {
+            this.valid = v;
+        });
 
-  }
+        this.successfulSubscription = this.switchBoard.successful$.subscribe((s) => {
+            this.successful = s;
+        });
 
-  ngOnDestroy(): void{
-    this.validSubscription.unsubscribe();
-    this.successfulSubscription.unsubscribe();
-  }
+    }
+
+    ngOnDestroy(): void {
+        this.validSubscription.unsubscribe();
+        this.successfulSubscription.unsubscribe();
+    }
 
 }
