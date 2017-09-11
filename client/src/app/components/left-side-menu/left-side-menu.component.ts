659728c7826e8478ef6e9b1711f5c20f0ebdaad8
Eduard <eduardg@kainos.com>
2017-09-11 17:04:15 +0100

    Adds user from cookie in the left side navigation.

--- client/src/app/components/left-side-menu/left-side-menu.component.ts
+++ client/src/app/components/left-side-menu/left-side-menu.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
-import { MenuStateService } from '../../services/menu-state.service';
-import { Subscription } from 'rxjs/Subscription';
-import { DataService } from '../../services/data.service';
-import { User } from '../../class/User';
-import { SwitchBoardService } from '../../services/switch-board.service';
+import {Component, Input, OnInit} from '@angular/core';
+import {trigger, state, style} from '@angular/animations';
+import {MenuStateService} from '../../services/menu-state.service';
+import {Subscription} from 'rxjs/Subscription';
+import {DataService} from '../../services/data.service';
+import {User} from '../../class/User';
+import {SwitchBoardService} from '../../services/switch-board.service';
 
 @Component({
     selector: 'evolve-left-side-menu',
@@ -22,17 +22,43 @@ import { SwitchBoardService } from '../../services/switch-board.service';
         ]),
     ]
 })
-export class LeftSideMenuComponent {
+export class LeftSideMenuComponent implements OnInit {
 
     @Input() menuState: string;
     private user: User = new User();
     private userSubscription: Subscription;
+    private menuStateSubscription: Subscription;
 
-    constructor(private data: DataService, private switchboard: SwitchBoardService) {
-        this.userSubscription = switchboard.user$.subscribe(user => this.user = user);
+    constructor(private data: DataService, private switchboard: SwitchBoardService, private menuStateService: MenuStateService) {
+        this.userSubscription = this.switchboard.user$.subscribe(user => this.user = user);
+        this.menuStateSubscription = this.menuStateService.menuState$.subscribe(menuState => this.menuState = menuState);
     }
 
     ngOnDestroy(): void {
+        if (this.userSubscription) {
             this.userSubscription.unsubscribe();
+        }
+        if (this.menuStateSubscription) {
+            this.menuStateSubscription.unsubscribe();
+        }
     }
+
+    ngOnInit(): void {
+        this.data.getUserFromCookie(this.user);
+        this.data.getUserInfoByUserID(this.user.userID).subscribe(
+            res => {
+                this.user.MRIN = res[0].MRIN;
+                this.user.address = res[0].address;
+                this.user.dateOfBirth = res[0].dateOfBirth;
+                this.user.email = res[0].email;
+                this.user.firstName = res[0].firstName;
+                this.user.gender = res[0].gender;
+                this.user.lastName = res[0].lastName;
+                this.user.phoneNumber = res[0].phoneNumber;
+                this.user.title = res[0].title;
+            },
+            err => console.log(err)
+        );
+    }
+
 }
