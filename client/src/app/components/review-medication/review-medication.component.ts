7fc48f3b4fca864571eff15c9043320d4f763373
Dariusz Jerzewski <d.jerzewski@kainos.com>
2017-09-11 16:53:23 +0100

    Fixed hardcoded userID in review-medication component@

--- client/src/app/components/review-medication/review-medication.component.ts
+++ client/src/app/components/review-medication/review-medication.component.ts
@@ -1,4 +1,4 @@
-import { Component, TemplateRef } from '@angular/core';
+import { Component, TemplateRef, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Medication } from '../../class/Medication';
 import { BsModalService } from 'ngx-bootstrap/modal';
@@ -8,24 +8,27 @@ import { MedicationComment } from '../../class/MedicationComment';
 import { MedicationDescription } from '../../class/MedicationDescription';
 import { Sanitizer } from '@angular/core';
 import { SecurityContext } from '@angular/core';
+import { User } from '../../class/User';
+import { SwitchBoardService } from '../../services/switch-board.service';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
     selector: 'evolve-review-medication',
     templateUrl: './review-medication.component.html',
     styleUrls: ['./review-medication.component.css']
 })
-export class ReviewMedicationComponent {
-
+export class ReviewMedicationComponent implements OnInit{
     public selectedMedication: Medication;
     public selectedMedicationComments: MedicationComment[];
     public selectedMedicationHistory: Medication[]
     public modalRef: BsModalRef;
     public medicationsList: Medication[];
-    public dataService: DataService;
     public newComment: string;
     public description: MedicationDescription;
     public sanitizer: Sanitizer;
     public collapsedDescription: boolean;
+    private user: User = new User();
+    private userSubscription: Subscription;
 
     public openModal(meds: Medication, template: TemplateRef<any>) {
         this.collapsedDescription = true;
@@ -37,9 +40,11 @@ export class ReviewMedicationComponent {
             res => this.selectedMedicationComments = res
         );
 
-        this.dataService.getMedicationHistory(this.selectedMedication.medicationID, 1).subscribe(
-            res => this.selectedMedicationHistory = res
-        );
+        if(this.user)
+        if(this.user.userID)
+            this.dataService.getMedicationHistory(this.selectedMedication.medicationID, this.user.userID).subscribe(
+                res => this.selectedMedicationHistory = res
+            );
 
         this.dataService.getWikiSummary(meds.medicationName).subscribe(
             res => { this.description = res; }
@@ -69,10 +74,16 @@ export class ReviewMedicationComponent {
         this.collapsedDescription = this.collapsedDescription == true ? false : true;
     }
 
-    constructor(dataService: DataService, private modalService: BsModalService) {
-        this.dataService = dataService;
-        dataService.getMedicationList(1).subscribe(
-            res => this.medicationsList = res
-        )
+    constructor(private dataService: DataService, private modalService: BsModalService, private switchboard: SwitchBoardService) {
+        this.switchboard.user$.subscribe(usr => this.user = usr);
+        this.dataService.getUserFromCookie(this.user);
+    }
+
+    ngOnInit(): void {
+        if(this.user)
+            if(this.user.userID)
+                this.dataService.getMedicationList(this.user.userID).subscribe(
+                    res => this.medicationsList = res
+                )
     }
 }
