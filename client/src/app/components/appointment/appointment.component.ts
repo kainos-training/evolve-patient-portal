78b331abd6979bdbee98209199a120a73d34d6b2
Eduard <eduardg@kainos.com>
2017-09-12 10:50:14 +0100

    Adds toggle button.

--- client/src/app/components/appointment/appointment.component.ts
+++ client/src/app/components/appointment/appointment.component.ts
@@ -1,7 +1,7 @@
 import {NavigatorGeolocation} from '@ngui/map';
 import {Subscription} from 'rxjs/Rx';
 import {AppointmentFurtherInfo} from '../../class/AppointmentFurtherInfo';
-import {Component, OnDestroy, OnInit, TemplateRef} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output, TemplateRef} from '@angular/core';
 import {BsModalService} from 'ngx-bootstrap/modal';
 import {BsModalRef} from 'ngx-bootstrap/modal/modal-options.class';
 import {Appointment} from '../../class/Appointment';
@@ -24,6 +24,10 @@ export class AppointmentComponent implements OnInit, OnDestroy {
     private modalRef: BsModalRef;
     private user: User = new User();
     private userSubscription: Subscription;
+
+    isLocalMapVisible: boolean = true;
+    @Output() open: EventEmitter<any> = new EventEmitter();
+    @Output() close: EventEmitter<any> = new EventEmitter();
 
     constructor(private modalService: BsModalService, private data: DataService, private switchboard: SwitchBoardService) {
         this.userSubscription = this.switchboard.user$.subscribe(user => {
@@ -58,5 +62,15 @@ export class AppointmentComponent implements OnInit, OnDestroy {
             this.userSubscription.unsubscribe();
         if(this.subCenter)
             this.subCenter.unsubscribe();
+    }
+
+    private toggle($event): void {
+        // console.log($event.path[1]);
+        this.isLocalMapVisible = !this.isLocalMapVisible;
+        if (this.isLocalMapVisible) {
+            this.open.emit(null);
+        } else {
+            this.close.emit(null);
+        }
     }
 }
