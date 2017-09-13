95749696987b2fe904993fe918b21a3c0ba7a901
Richard Taylor <richardta@kainos.com>
2017-09-13 16:10:15 +0100

    added login functions to a couple of medicationControllerTest functions

--- server/test/integration/medicationControllerTest.js
+++ server/test/integration/medicationControllerTest.js
@@ -1,11 +1,13 @@
 var request = require('supertest');
-let chai = require('chai');
+var chai = require('chai');
+var async = require('async');
 
-let should = chai.should();
+var should = chai.should();
 var assert = chai.assert;
 
 describe('Medication Endpoints', function() {
     var app;
+
     beforeEach(function() {
         app = require('../../index');
     });
@@ -14,22 +16,76 @@ describe('Medication Endpoints', function() {
     });
     describe('Test /medication/list', function() {
         it('returns valid data and status code 200 when posting valid data', function(done) {
-            request(app)
-                .post('/medication/list')
-                .type('json')
-                .set('Accept', 'application/json')
-                .send('{"userID":1}')
-                .expect(200, done);
+            async.waterfall(
+                [
+                    login,
+                    async.apply(testMedicationListRoute),
+                ],
+                function finish(err, result) {
+                    done(err);
+                }
+            );
+
+            function login(callback) {
+                var user = {
+                    username: 'jsmith',
+                    password: 'password123'
+                };
+            
+                request(app)
+                    .post('/auth/login')
+                    .send(user)
+                    .expect(200)
+                    .end(function(err, res) {
+                        if (err) return callback(err);
+                        callback(null, res.body.token);
+                    });
+                callback(null, 'one')
+            }
+
+            function testMedicationListRoute(token, callback) {
+                request(app)
+                    .post('/medication/list')
+                    .set('x-access-token', token)
+                    .type('json')
+                    .send('{"userID":1}')
+                    .expect(200, done);
+                callback(null, 'done');
+            }
         });
 
         it('returns success false and status code 400 when posting invalid data', function(done) {
-            request(app)
-                .post('/medication/list')
-                .expect(400)
-                .expect(function(res) {
-                    res.body.success.should.equal(false);
-                })
-                .end(done);
+            async.waterfall(
+                [
+                    function login(callback) {
+                        var user = {
+                            username: 'jsmith',
+                            password: 'password123'
+                        };
+
+                        request(app)
+                            .post('/auth/login')
+                            .send(user)
+                            .expect(200)
+                            .end(function(err, res) {
+                                if (err) return callback(err);
+                                callback(null, res.body.token);
+                            });
+                    },
+                    function testMedicationListRoute(token, callback) {
+                        request(app)
+                            .post('/medication/list')
+                            .expect(403)
+                            .expect(function(res) {
+                                res.body.success.should.equal(false);
+                            })
+                            .end(done);
+                    }
+                ],
+                function finish(err, result) {
+                    done(err);
+                }
+            );
         });
     });
 
