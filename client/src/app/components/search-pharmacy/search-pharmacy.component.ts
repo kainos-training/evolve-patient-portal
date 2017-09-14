fb41cda637e1ac1bf1415cd45787a783f865e716
Jordan Thomson <j.thomson@kainos.com>
2017-09-14 13:28:07 +0100

    initial autocomplete implementation

--- client/src/app/components/search-pharmacy/search-pharmacy.component.ts
+++ client/src/app/components/search-pharmacy/search-pharmacy.component.ts
@@ -1,4 +1,7 @@
+import { document } from 'ngx-bootstrap/utils/facade/browser';
 import { Component, OnInit } from '@angular/core';
+import { googlemaps } from 'google-maps';
+import { } from '@types/googlemaps';
 
 @Component({
     selector: 'evolve-search-pharmacy',
@@ -11,4 +14,21 @@ export class SearchPharmacyComponent implements OnInit {
 
     ngOnInit() {
     }
+
+    onInputChange($event) {
+        // enters here when input is detected
+
+        var input = <HTMLInputElement>document.getElementById('searchTextField');
+
+        if (input) {
+            var options = {
+                types: [
+                    '(regions)'
+                ],
+            };
+            // var autocomplete = new google.maps.places.Autocomplete(input, options);
+            var autocomplete = new google.maps.places.Autocomplete(input,options);
+        }
+    }
 }
+
