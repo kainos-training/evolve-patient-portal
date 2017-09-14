49f942f4e504a54ef42867c4f9ec336567dbbb78
shannond <shannond@kainos.com>
2017-09-14 14:35:39 +0100

    Further changes for pull request

--- client/src/app/components/side-effects/side-effects.component.spec.ts
+++ client/src/app/components/side-effects/side-effects.component.spec.ts
@@ -28,112 +28,103 @@ import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 
 export class MockDataService {
     userSideEffects: Array<SideEffect>;
-    constructor(){
+    constructor() {
         this.userSideEffects = new Array<SideEffect>();
-        this.userSideEffects.push(new SideEffect(1,1,"Headaches","",false));
+        this.userSideEffects.push(new SideEffect(1, 1, "Headaches", "", false));
     }
-    getCookie(){
+    getCookie() {
         return "1";
     }
 
-  getUserSideEffects(userID: number) {
-    if (userID == 1) {
-      return this.userSideEffects
+    getUserSideEffects(userID: number) {
+        if (userID == 1) {
+            return this.userSideEffects
+        }
+        return [];
     }
-    return [];
-  }
-  addUserSideEffect(userID: number, commentText: String){
-      this.userSideEffects.push(new SideEffect(2,userID,commentText,"", false));
-  }
+    addUserSideEffect(userID: number, commentText: String) {
+        this.userSideEffects.push(new SideEffect(2, userID, commentText, "", false));
+    }
 }
 
 export class MockSwitchboardService {
     getUser(userID: number) {
-        let u : User;
+        let u: User;
         return u;
     }
 }
 
 export class MockBSModalService {
-    show()
-    {
+    show() {
         return new BsModalRef();
     }
 }
 
 describe('SideEffectsComponent', () => {
-  let component: SideEffectsComponent;
-  let fixture: ComponentFixture<SideEffectsComponent>;
-  let mockDataService: MockDataService;
+    let component: SideEffectsComponent;
+    let fixture: ComponentFixture<SideEffectsComponent>;
+    let mockDataService: MockDataService;
 
-  beforeEach(async(() => {
-    mockDataService = new MockDataService();
-    TestBed.configureTestingModule({
-        declarations: [
-            SideEffectsComponent, 
-            EllipsisPipe, 
-            LoginComponent, 
-            ErrorPageComponent, 
-            DashboardComponent, 
-            SettingNewPasswordComponent, 
-            RequestPasswordResetComponent],
-        providers: [
-            DataService, 
-            HttpClientModule, 
-            BsModalService, 
-            CookieService, 
-            SwitchBoardService, 
-            RouteGuard, 
-            {provide: APP_BASE_HREF, useValue: '/'}],
-        imports: [
-            routes,
-            ModalModule.forRoot(),
-            BrowserModule,
-            HttpClientModule,
-            FormsModule,
-            AlertModule.forRoot()
-        ],
-        schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    })
-        .compileComponents();
-}));
+    beforeEach(async(() => {
+        mockDataService = new MockDataService();
+        TestBed.configureTestingModule({
+            declarations: [
+                SideEffectsComponent,
+                EllipsisPipe,
+                LoginComponent,
+                ErrorPageComponent,
+                DashboardComponent,
+                SettingNewPasswordComponent,
+                RequestPasswordResetComponent],
+            providers: [
+                DataService,
+                HttpClientModule,
+                BsModalService,
+                CookieService,
+                SwitchBoardService,
+                RouteGuard,
+                { provide: APP_BASE_HREF, useValue: '/' }],
+            imports: [
+                routes,
+                ModalModule.forRoot(),
+                BrowserModule,
+                HttpClientModule,
+                FormsModule,
+                AlertModule.forRoot()
+            ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA]
+        })
+            .compileComponents();
+    }));
 
     TestBed.overrideComponent(SideEffectsComponent, {
         set: {
             providers: [
-                {provide: DataService, useClass: MockDataService},
-                {provide: SwitchBoardService, useClass: MockSwitchboardService},
-                {provide: BsModalService, useClass: MockBSModalService}
+                { provide: DataService, useClass: MockDataService },
+                { provide: SwitchBoardService, useClass: MockSwitchboardService },
+                { provide: BsModalService, useClass: MockBSModalService }
             ]
         }
     });
 
-  it('should be created', async() => {
-    fixture = TestBed.createComponent(SideEffectsComponent);
-    component = fixture.componentInstance;
-    expect(component).toBeTruthy();
-  });
+    it('should be created', async () => {
+        fixture = TestBed.createComponent(SideEffectsComponent);
+        component = fixture.componentInstance;
+        expect(component).toBeTruthy();
+    });
 
-  it('should get the cookie from the data service', async() => {
-    fixture = TestBed.createComponent(SideEffectsComponent);
-    component = fixture.componentInstance;
-    expect(this.mockDataService.getCookie()).toBe("1");
-  });
+    it('should get the cookie from the data service', async () => {
+        fixture = TestBed.createComponent(SideEffectsComponent);
+        component = fixture.componentInstance;
+        expect(this.mockDataService.getCookie()).toBe("1");
+    });
 
-  it('should change showSideEffects boolean from true to false', async () => {
-    fixture = TestBed.createComponent(SideEffectsComponent);
-    component = fixture.componentInstance;
-    component.showSideEffects = true;
-    component.selectShowSideEffects();
-    expect(component.showSideEffects).toBe(false);
-});
-
-it('should remove ', async () => {
-  fixture = TestBed.createComponent(SideEffectsComponent);
-  component = fixture.componentInstance;
-  component.showSideEffects = true;
-  component.selectShowSideEffects();
-  expect(component.showSideEffects).toBe(false);
-});
+    it('should change showSideEffects boolean from true to false', async () => {
+        fixture = TestBed.createComponent(SideEffectsComponent);
+        component = fixture.componentInstance;
+        component.showSideEffects = true;
+        component.selectShowSideEffects();
+        expect(component.showSideEffects).toBe(false);
+    });
 
 });
