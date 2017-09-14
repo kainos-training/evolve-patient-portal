7fd97b01e37579de39118c4e7a3380524b9a74bc
Mary-Jane <mary-janemc@kainos.com>
2017-09-14 10:06:17 +0100

    Fixed tests that always pass even when broken in the side effects component spec

--- client/src/app/components/side-effects/side-effects.component.spec.ts
+++ client/src/app/components/side-effects/side-effects.component.spec.ts
@@ -1,43 +1,66 @@
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
-import { ConditionComponent } from '../condition/condition.component';
-import { Component, OnInit } from '@angular/core';
-import { Condition } from '../../class/Condition';
-import { User } from '../../class/User';
-import { SwitchBoardService } from '../../services/switch-board.service';
-import { Subscription } from 'rxjs/Rx';
-import { DataService } from '../../services/data.service';
-import { HttpClientModule, HttpClient, HttpHandler } from '@angular/common/http';
-import { CookieService } from 'ngx-cookie-service';
-import { routes } from '../../app.router';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { LoginComponent } from '../login/login.component';
-import { ErrorPageComponent } from '../error-page/error-page.component';
-import { DashboardComponent } from '../dashboard/dashboard.component';
-import { TopBarComponent } from '../top-bar/top-bar.component';
-import { LeftSideMenuComponent } from '../left-side-menu/left-side-menu.component';
-import { AppointmentComponent } from '../appointment/appointment.component';
-import { SettingNewPasswordComponent } from '../setting-new-password/setting-new-password.component';
-import { RequestPasswordResetComponent } from '../request-password-reset/request-password-reset.component';
-import { ReviewMedicationComponent } from '../review-medication/review-medication.component';
-import { MapViewComponent } from '../map-view/map-view.component';
-import { EllipsisPipe } from '../../utils/ellipsis.pipe';
-import { Marker, NguiMapComponent, DirectionsRenderer } from '@ngui/map/dist';
-import { APP_BASE_HREF } from '@angular/common';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { MyTasksComponent } from '../my-tasks/my-tasks.component';
-import { SideEffectsComponent } from './side-effects.component';
-import { SideEffect } from '../../class/SideEffect';
+import {BrowserModule} from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {AlertModule} from 'ngx-bootstrap';
+import {DataService} from '../../services/data.service';
+import {HttpClientModule, HttpClient, HttpHandler} from '@angular/common/http';
+import {SideEffectsComponent} from './side-effects.component';
+import {SwitchBoardService} from '../../services/switch-board.service';
+import {ModalModule} from 'ngx-bootstrap/modal';
+import {EllipsisPipe} from '../../utils/ellipsis.pipe';
+import {LoginComponent} from '../login/login.component';
+import {CookieService} from 'ngx-cookie-service';
+import {routes} from '../../app.router';
+import {ErrorPageComponent} from '../error-page/error-page.component';
+import {RouteGuard} from '../../services/route.guard';
+import {TopBarComponent} from '../top-bar/top-bar.component';
+import {LeftSideMenuComponent} from '../left-side-menu/left-side-menu.component';
+import {AppointmentComponent} from '../appointment/appointment.component';
+import {DashboardComponent} from '../dashboard/dashboard.component';
+import {SideEffect} from '../../class/SideEffect';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {BsModalService} from 'ngx-bootstrap/modal';
+import {SettingNewPasswordComponent} from '../setting-new-password/setting-new-password.component';
+import {RequestPasswordResetComponent} from '../request-password-reset/request-password-reset.component';
+import {APP_BASE_HREF} from '@angular/common';
+import {User} from '../../class/User';
+import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 
 export class MockDataService {
-  getUserSideEffects(userID: number) {
-    let toReturn: Array<SideEffect> = [];
-    if (userID == 1) {
-      
+    userSideEffects: Array<SideEffect>;
+    constructor(){
+        this.userSideEffects = new Array<SideEffect>();
+        this.userSideEffects.push(new SideEffect(1,1,"Headaches","",false));
     }
-    return toReturn;
+    getCookie(){
+        return "1";
+    }
+
+  getUserSideEffects(userID: number) {
+    if (userID == 1) {
+      return this.userSideEffects
+    }
+    return [];
   }
+  addUserSideEffect(userID: number, commentText: String){
+      this.userSideEffects.push(new SideEffect(2,userID,commentText,"", false));
+  }
+}
+
+export class MockSwitchboardService {
+    getUser(userID: number) {
+        let u : User;
+        return u;
+    }
+
+
+}
+
+export class MockBSModalService {
+    show()
+    {
+        return new BsModalRef();
+    }
 }
 
 describe('SideEffectsComponent', () => {
@@ -49,47 +72,56 @@ describe('SideEffectsComponent', () => {
     mockDataService = new MockDataService();
     TestBed.configureTestingModule({
         declarations: [
-            ConditionComponent,
-            LoginComponent,
-            ErrorPageComponent,
-            DashboardComponent,
-            TopBarComponent,
-            LeftSideMenuComponent,
-            AppointmentComponent,
-            SettingNewPasswordComponent,
-            RequestPasswordResetComponent,
-            ReviewMedicationComponent,
-            MapViewComponent,
-            EllipsisPipe,
-            Marker,
-            DirectionsRenderer,
-            NguiMapComponent,
-            MyTasksComponent,
-            SideEffectsComponent
-        ],
+            SideEffectsComponent, 
+            EllipsisPipe, 
+            LoginComponent, 
+            ErrorPageComponent, 
+            DashboardComponent, 
+            SettingNewPasswordComponent, 
+            RequestPasswordResetComponent],
         providers: [
-            DataService,
-            HttpClient,
-            HttpHandler,
-            HttpClientModule,
-            CookieService,
-            SwitchBoardService,
-            { provide: APP_BASE_HREF, useValue: '/' }
-        ],
+            DataService, 
+            HttpClientModule, 
+            BsModalService, 
+            CookieService, 
+            SwitchBoardService, 
+            RouteGuard, 
+            {provide: APP_BASE_HREF, useValue: '/'}],
         imports: [
             routes,
-            BrowserModule,
             ModalModule.forRoot(),
+            BrowserModule,
+            HttpClientModule,
             FormsModule,
-            TooltipModule.forRoot()
-        ]
+            AlertModule.forRoot()
+        ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
         .compileComponents();
 }));
+
+    TestBed.overrideComponent(SideEffectsComponent, {
+        set: {
+            providers: [
+                {provide: DataService, useClass: MockDataService},
+                {provide: SwitchBoardService, useClass: MockSwitchboardService},
+                {provide: BsModalService, useClass: MockBSModalService}
+            ]
+        }
+    });
 
   it('should be created', async() => {
     fixture = TestBed.createComponent(SideEffectsComponent);
     component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  it('should add a new side effect to appropriate user', async() => {
+    fixture = TestBed.createComponent(SideEffectsComponent);
+    component = fixture.componentInstance;
+    component.newSideEffect = "I can't sleep";
+    component.addSideEffect();
+    let testSideEffect: SideEffect = component.userSideEffects.pop();
+    expect(testSideEffect.sideEffectText).toBe("I can't sleep");
   });
 });
