f19f2a6f8944dc3ca2339890dd3ce882ce5278c0
John Doherty <johnjmdoherty@gmail.com>
2017-12-05 13:39:19 +0000

    Test updated

--- client/src/app/components/top-bar/top-bar.component.spec.ts
+++ client/src/app/components/top-bar/top-bar.component.spec.ts
@@ -1,34 +1,15 @@
-import {BsModalService, ComponentLoaderFactory, PositioningService} from 'ngx-bootstrap';
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {TopBarComponent} from './top-bar.component';
-import {PersonalInfoHeaderComponent} from '../personal-info-header/personal-info-header.component';
-import {MenuStateService} from '../../services/menu-state.service';
-import {DataService} from '../../services/data.service';
-import {Router} from '@angular/router';
-import {FormsModule} from '@angular/forms';
-import {SwitchBoardService} from '../../services/switch-board.service';
-import { Observable } from 'rxjs/Rx';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TopBarComponent } from './top-bar.component';
+import { FormsModule } from '@angular/forms';
 
 describe('TopBarComponent', () => {
     let component: TopBarComponent;
     let fixture: ComponentFixture<TopBarComponent>;
-    const mockDataService = {};
-    const mockRouter = {};
-   // const mockSwitchBoard = {};
-   // const mockObservable = {};
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [TopBarComponent, PersonalInfoHeaderComponent],
-            imports:[],
-            providers: [
-                MenuStateService,
-                BsModalService,
-                ComponentLoaderFactory,
-                PositioningService,
-               SwitchBoardService,
-                {provide: Router, useValue: mockRouter}
-            ]
+            imports: [ FormsModule ],
+            declarations: [ TopBarComponent ]
         }).compileComponents();
     }));
 
@@ -40,10 +21,5 @@ describe('TopBarComponent', () => {
 
     it('should be created', () => {
         expect(component).toBeTruthy();
-    });
-
-    it('sideMenu state should be out/in depending on the window size', () => {
-        var menuState = component.getMenuState();
-        expect(menuState).toEqual('out');
     });
 });
