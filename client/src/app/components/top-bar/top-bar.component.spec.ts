78472ca2b8b95abd9c290c3dd28216742d2a1556
John Doherty <johnjmdoherty@gmail.com>
2017-12-05 14:20:57 +0000

    Bat and ball

--- client/src/app/components/top-bar/top-bar.component.spec.ts
+++ client/src/app/components/top-bar/top-bar.component.spec.ts
@@ -1,15 +1,36 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TopBarComponent } from './top-bar.component';
 import { FormsModule } from '@angular/forms';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { PersonalInfoHeaderComponent } from '../personal-info-header/personal-info-header.component';
+import { DataService } from '../../services/data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { SwitchBoardService } from '../../services/switch-board.service';
+import { Router } from '@angular/router';
+import {MenuStateService} from "../../services/menu-state.service";
+import {BsModalRef, ComponentLoaderFactory, PositioningService} from "ngx-bootstrap";
+import { BsModalService } from 'ngx-bootstrap/modal';
 
-describe('TopBarComponent', () => {
+fdescribe('TopBarComponent', () => {
     let component: TopBarComponent;
     let fixture: ComponentFixture<TopBarComponent>;
 
+    const mockRouter = {};
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [ FormsModule ],
-            declarations: [ TopBarComponent ]
+            imports: [ FormsModule, HttpClientModule ],
+            declarations: [TopBarComponent,
+                PersonalInfoHeaderComponent],
+            providers: [DataService,
+                CookieService,
+                SwitchBoardService,
+                { provide: Router, useValue: mockRouter },
+                MenuStateService,
+                BsModalRef,
+                BsModalService,
+                ComponentLoaderFactory,
+                PositioningService]
         }).compileComponents();
     }));
 
