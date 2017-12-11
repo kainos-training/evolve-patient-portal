92226e1258fd91bb17c30b9c34ee46411ca3c58f
Micheal Corr <m.corr@kainos.com>
2017-12-11 11:57:39 +0000

    Emailer added

--- server/emailNotifier.js
+++ server/emailNotifier.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 
 exports.sendEmail = function (recipient, notificationType, emailBody) {
     var isTrueSet = (process.env.secure == 'true');
-    nodemailer.sendNotification((err, recipient, notificationType, emailBody) => {
+    nodemailer.createTestAccount((err, recipient, notificationType, emailBody) => {
 
         let transporter = nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
@@ -11,14 +11,14 @@ exports.sendEmail = function (recipient, notificationType, emailBody) {
             secure: isTrueSet,
             requireTLS: true,
             auth: {
-                user = "kainostdp2017@gmail.com",
-                pass = 'k@in0stdp'
+                user: "kainostdp2017@gmail.com",
+                pass: 'k@in0stdp'
             }
         });
 
         let mailOptions = {
             from: '"Evolve Portal" <kainostdp2017@gmail.com>',
-            to: recipient,
+            to: 'm.corr@kainos.com', //recipient, (passed paramteres arnt working yet and no recipent causes othe rissues so hardcoding for now)
             subject: "Notification: " + notificationType,
             text: emailBody//,
             //html: (can define body using html instead of plain text)
