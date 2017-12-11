95c90e615c5eb6d8b507a200910e18e4b7960a47
Calum-Forster <c.forster@kainos.com>
2017-12-11 15:25:39 +0000

    Making it look nicer

--- client/src/app/components/generate-appointment/generate-appointment.component.ts
+++ client/src/app/components/generate-appointment/generate-appointment.component.ts
@@ -7,15 +7,21 @@ import { DataService } from '../../services/data.service';
   styleUrls: ['./generate-appointment.component.css']
 })
 export class GenerateAppointmentComponent implements OnInit {
+  date:Date;
+  dateString:String;
+  timeString:String;
 
   constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.timeString = '12:00:00';
   }
 
   onSubmit(){
-
-    this.data.addAppointment(null, 1, 1, 1, '2018-11-15 12:15:05', "Hurt Arm", 1);
+    this.date = new Date(this.dateString+' '+this.timeString);
+    
+      console.log(this.date);
+    this.data.addAppointment(null, 1, 1, 1, '2017-12-25 12:00:00', "Hurt Face", 1);
   }
 
 }
