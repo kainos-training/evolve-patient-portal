d59c1f16ad4cb85ae87dcdb69c97d01b32661bcf
Síona Murray <s.murray@kainos.com>
2017-12-12 14:56:35 +0000

    Karma update

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -34,14 +34,15 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     customLaunchers: {
-      Chrome: {
+      ChromeHeadless: {
         base: 'Chrome',
         flags: [
           // See https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md,
-          '--disable-gpu',
+        '--headless',  
+	'--disable-gpu',
           // Without a remote debugging port, Google Chrome exits immediately.
           '--remote-debugging-port=9222',
         ],
