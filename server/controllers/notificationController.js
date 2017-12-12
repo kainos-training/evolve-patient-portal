bfb91abaf031f3332e70111af996d7dab317645a
Micheal Corr <m.corr@kainos.com>
2017-12-12 10:20:50 +0000

    Start of notification scheduler

---
+++ server/controllers/notificationController.js
@@ -0,0 +1,3 @@
+const emailer = require("../emailer");
+
+emailer.sendNotification(emailAddress, name, id, type, querySubject, queryText, notificationType, htmlBody);
--- server/emailer.js
+++ server/emailer.js
@@ -2,7 +2,7 @@
 // the user who booked into the event
 const nodemailer = require('nodemailer');
 
-exports.sendNotification = function(emailAddress, name, id, type, querySubject, queryText) {
+exports.sendNotification = function(emailAddress, name, id, type, querySubject, queryText, notificationType, htmlBody) {
     var isTrueSet = (process.env.SECURE == 'true');
     nodemailer.createTestAccount((err, account) => {
         // create reusable transporter object using the default SMTP transport
@@ -17,7 +17,7 @@ exports.sendNotification = function(emailAddress, name, id, type, querySubject, 
             }
         });
         let mailOptions = {}
-        if(type=="reset"){
+        if(type == "reset"){
             // setup email data with unicode symbols
             mailOptions = {
                 from: '"Evolve Patient" <kainostdp2017@gmail.com>', // sender address
@@ -28,15 +28,23 @@ exports.sendNotification = function(emailAddress, name, id, type, querySubject, 
                 html: 'Click the following link to reset your password: <a href="http://localhost:4200/reset/' + id + '">Reset Password</a><br/>' +
                     '<br/><b style="color:red"><u>If You Did NOT</u></b> request this password reset please report this To Kainos Immediately' // html body
             };
-        }else{
+        }else if(type == "notify"){
             mailOptions = {
-                from: '"Evolve Patient" <kainostdp2017@gmail.com>', // sender address
+                from: '"Evolve Portal" <kainostdp2017@gmail.com>', // sender address
                 to: emailAddress, // list of receivers
-                subject: querySubject, // Subject line
+                subject: "Notification: " + querySubject, // Subject line
                 text: queryText, // plain text body
-                html: queryText // html body
+                html: htmlBody
             };
-        }
+        }else {
+                mailOptions = {
+                    from: '"Evolve Portal" <kainostdp2017@gmail.com>', // sender address
+                    to: emailAddress, // list of receivers
+                    subject: "Notification: " + querySubject, // Subject line
+                    text: queryText, // plain text body
+                    html: htmlBody
+                };
+            }
 
         // send notification to users email address
         // send mail with defined transport object
