09eee415a8aed52c80969befbf9004c7f8ca7254
John Doherty <johnjmdoherty@gmail.com>
2017-12-12 13:47:37 +0000

    Chrome changes

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -40,7 +40,7 @@ module.exports = function (config) {
         customLaunchers: {
             Chrome_travis_ci: {
                 base: 'Chrome',
-                flags: []
+                flags: [' — no-sandbox']
             }
         }
     };
