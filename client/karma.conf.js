7fd9237eaf618cf0c9d68cd4edad0aff6d08c281
John Doherty <johnjmdoherty@gmail.com>
2017-12-12 13:03:44 +0000

    Travis update

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -37,15 +37,6 @@ module.exports = function (config) {
         autoWatch: true,
         browsers: ['Chrome'],
         singleRun: true,
-        customLaunchers: {
-            Chrome_travis_ci: {
-                base: 'Chrome',
-                flags: ['--no-sandbox']
-            }
-        },
     };
-    if(process.env.TRAVIS){
-        configuration.browsers = ['Chrome_travis_ci'];
-    }
     config.set(configuration);
 };
