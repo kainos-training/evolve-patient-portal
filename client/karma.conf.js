a313ea1bb4b5beb89cbdc1792b79ec79ac812a9d
Conor McCourt <c.mccourt@kainos.com>
2017-12-12 14:55:56 +0000

    Removing headless

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function(config) {
         colors: false,
         logLevel: config.LOG_DEBUG,
         autoWatch: true,
-        browsers: ['ChromeHeadless'],
+        browsers: ['Chrome'],
         singleRun: true,
     };
     config.set(configuration);
