6e8621adeba3834abd2aafb8827e6eb0f74ecbe2
Deaglan Marken <deaglanm@systemss-MBP-5.home>
2017-12-14 23:33:12 +0000

    Made changes to userInfoController.js to fix test fail

--- server/controllers/userInfoController.js
+++ server/controllers/userInfoController.js
@@ -4,7 +4,7 @@ exports.getUserInfoByUserID = function(req, res) {
     let userID = req.body.userID;
     db.query(
         
-        "SELECT userID, firstName, lastName, dateOfBirth, mobilePhoneNumber, homePhoneNumber, workPhoneNumber, title, gender, MRIN, address, email, user.gpID, GP.gpFullName, GPPractice.gpPracticeName " +
+        "SELECT userID, firstName, lastName, dateOfBirth, mobilePhoneNumber, homePhoneNumber, workPhoneNumber, title, gender, MRIN, address, email, User.gpID, GP.gpFullName, GPPractice.gpPracticeName " +
         "FROM User " +
         "INNER JOIN GP ON User.gpID = GP.gpID " +
         "INNER JOIN GPPractice ON GP.gpPracticeID=GPPractice.gpPracticeID "+
