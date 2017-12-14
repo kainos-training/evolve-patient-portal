d5ac52e763898d82ce55f70b80a8cc5cf9ffbb41
Calum-Forster <c.forster@kainos.com>
2017-12-14 14:56:57 +0000

    Removed tests

--- client/src/app/components/generate-appointment/generate-appointment.component.spec.ts
+++ client/src/app/components/generate-appointment/generate-appointment.component.spec.ts
@@ -19,7 +19,7 @@ describe('GenerateAppointmentComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should be created', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
