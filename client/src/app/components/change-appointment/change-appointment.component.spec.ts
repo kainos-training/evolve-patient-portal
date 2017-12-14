1f71a991b114d3e6c5bd3ca8ab1369480f46fac4
Lauren Burns <laurenb@kainos.com>
2017-12-14 18:02:39 +0000

    Feature/013/clinician change appointment (#78)
    * Clinician change appointment details page added
    
    * Clinician change appointment details page added
    
    * changes made to appearance of change appointment form
    
    * Updates to the change appointment demo page
    
    * added delete button to query
    
    * Story 13 complete, change/delete appointment added
    
    * merge with develop
    
    * tests fixed
    
    * Fixing failed tests
    
    * demo sql script added
    
    * fixing merge conflicts x2

---
+++ client/src/app/components/change-appointment/change-appointment.component.spec.ts
@@ -0,0 +1,20 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ChangeAppointmentComponent } from './change-appointment.component';
+
+describe('GenerateAppointmentComponent', () => {
+  let component: ChangeAppointmentComponent;
+  let fixture: ComponentFixture<ChangeAppointmentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ChangeAppointmentComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChangeAppointmentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+});
