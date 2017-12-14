1efc39711ecc23702cfbff31d063c49494acf3c9
Calum-Forster <c.forster@kainos.com>
2017-12-14 16:51:52 +0000

    Adding the router

--- client/src/app/app.router.ts
+++ client/src/app/app.router.ts
@@ -7,6 +7,7 @@ import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {SettingNewPasswordComponent} from './components/setting-new-password/setting-new-password.component';
 import {RequestPasswordResetComponent} from './components/request-password-reset/request-password-reset.component';
 import {QuestionnaireComponent} from './components/questionnaire/questionnaire.component';
+import {GenerateAppointmentComponent} from './components/generate-appointment/generate-appointment.component';
 
 export const router: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -16,7 +17,9 @@ export const router: Routes = [
     {path: 'reset/:id', component: SettingNewPasswordComponent},
     {path: 'request-reset', component: RequestPasswordResetComponent},
     {path: 'questionnaire', component: QuestionnaireComponent, canActivate: [RouteGuard]},
-    {path: '**', component: LoginComponent}
+    {path: 'genAppointment', component: GenerateAppointmentComponent},
+    {path: '**', component: LoginComponent},
+    
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
